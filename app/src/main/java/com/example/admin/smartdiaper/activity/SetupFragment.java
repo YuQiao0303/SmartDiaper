05a140189ce40c16a23843ae41fc77f07af543b4
YuQiao0303 <691761071@qq.com>
2019-04-04 12:41:43 +0800

    add ring volume setting preference
    add seekbar preference for ring volume using a lib

--- app/src/main/java/com/example/admin/smartdiaper/activity/SetupFragment.java
+++ app/src/main/java/com/example/admin/smartdiaper/activity/SetupFragment.java
@@ -2,6 +2,7 @@ package com.example.admin.smartdiaper.activity;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.view.ViewGroup;
 
 import com.example.admin.smartdiaper.R;
 
+import me.zhanghai.android.seekbarpreference.SeekBarPreference;
 
 public class SetupFragment extends PreferenceFragmentCompat {
 
@@ -22,6 +24,13 @@ public class SetupFragment extends PreferenceFragmentCompat {
         getPreferenceManager().setSharedPreferencesName("mysetting");
         addPreferencesFromResource(R.xml.preference_settings);
 
+    }
+
+    @Override
+    public void onDisplayPreferenceDialog(Preference preference) {
+        if (!SeekBarPreference.onDisplayPreferenceDialog(this, preference)) {
+            super.onDisplayPreferenceDialog(preference);
+        }
     }
 
 //    @Override
